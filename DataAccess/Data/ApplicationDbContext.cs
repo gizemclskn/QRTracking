f5c73107c83943b2ab59d05792c3aac0f360f238
Gizem Çalışkan <gizemcaliskan24@gmail.com>
2024-10-21 16:54:11 +0300

    Add ApplicationDbContext, AttendanceRecord, and Employee entities
    - Created ApplicationDbContext for database context management.
    - Added AttendanceRecord entity to track attendance data.
    - Added Employee entity to represent employee information.

---
+++ DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<AttendanceRecord> AttendanceRecords { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().HasKey(e => e.Id);
+            modelBuilder.Entity<AttendanceRecord>().HasKey(a => a.Id);
+        }
+    }
---
+++ Entities/AttendanceRecord.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class AttendanceRecord
+    {
+        public Guid Id { get; set; }
+        public Guid EmployeeId { get; set; }
+        public DateTime EntryTime { get; set; }
+        public DateTime? ExitTime { get; set; }
+    }
+}
